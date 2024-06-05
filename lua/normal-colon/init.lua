083f49c8339ec72d29937513db51d3a5b7b4f391
edte <zzzzip6@gmail.com>
2024-06-05 15:01:15 +0800

    init

--- lua/normal-colon/init.lua
+++ lua/normal-colon/init.lua
@@ -3,13 +3,7 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 
-	vim.keymap.set("n", "<Leader>h", function()
-		if opts.name then
-			print("hello, " .. opts.name)
-		else
-			print("hello")
-		end
-	end)
+	print("hello")
 end
 
 return M
